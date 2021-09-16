10f0c1286d73dbb5a6822497ba35dca93eb986b9
HellCatVN <hellcatvn@gmail.com>
2021-09-16 17:42:13 +0700

    Add bot send message

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
-const { MessageEmbed, WebhookClient } = require('discord.js');
-
-const channelId = '';
-const token = '';
-const hook = new WebhookClient({ id: channelId, token: token });
-
-const embed = new MessageEmbed()
-  .setColor('#0099ff')
-  .setTitle('Some title')
-  .setURL('https://discord.js.org/')
-  .setAuthor(
-    'Some name',
-    'https://i.imgur.com/AfFp7pu.png',
-    'https://discord.js.org'
-  )
-  .setDescription('Some description here')
-  .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-  .addFields(
-    { name: 'Regular field title', value: 'Some value here' },
-    { name: '\u200B', value: '\u200B' },
-    { name: 'Inline field title', value: 'Some value here', inline: true },
-    { name: 'Inline field title', value: 'Some value here', inline: true }
-  )
-  .addField('Inline field title', 'Some value here', true)
-  .setImage('https://i.imgur.com/AfFp7pu.png')
-  .setTimestamp()
-  .setFooter('Some footer text here', 'https://i.imgur.com/AfFp7pu.png');
-
-hook.send({
-  // content: 'Webhook test',
-  // username: 'some-username',
-  // avatarURL: 'https://i.imgur.com/AfFp7pu.png',
-  embeds: [embed],
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
+
+const channelID = '';
+const token = '';
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', async (message) => {
+  const channel = client.channels.cache.get(channelID);
+  if (message.content === 'send') {
+    const embed = new MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle('Some title')
+      .setURL('https://discord.js.org/')
+      .setAuthor(
+        'Some name',
+        'https://i.imgur.com/AfFp7pu.png',
+        'https://discord.js.org'
+      )
+      .setDescription('Some description here')
+      .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+      .addFields(
+        { name: 'Regular field title', value: 'Some value here' },
+        { name: '\u200B', value: '\u200B' },
+        { name: 'Inline field title', value: 'Some value here', inline: true },
+        { name: 'Inline field title', value: 'Some value here', inline: true }
+      )
+      .addField('Inline field title', 'Some value here', true)
+      .setImage('https://i.imgur.com/AfFp7pu.png')
+      .setTimestamp()
+      .setFooter('Some footer text here', 'https://i.imgur.com/AfFp7pu.png');
+
+    channel.send({ embeds: [embed] });
+  }
+});
+
+client.login(token);
